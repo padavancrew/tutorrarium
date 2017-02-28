838f922ec8a2d9030d800cf406b2d9c21dd2ddb6
oleksii <oleksii.kukuruza@gmail.com>
2017-02-28 02:17:32 +0200

    Configured heroku deployment

--- webpack.config.js
+++ webpack.config.js
@@ -1,56 +1,69 @@
 const path = require('path');
 const webpack = require('webpack');
+const HtmlWebpackPlugin = require('html-webpack-plugin');
 
-module.exports = {
-    entry: [
-        'react-hot-loader/patch',
-        'webpack-dev-server/client?http://localhost:8080',
-        'webpack/hot/only-dev-server',
-        './index.js'
-    ],
-    output: {
-        filename: 'bundle.js',
-        path: path.join(__dirname, 'dist'),
-        publicPath: '/'
-    },
-    context: path.join(__dirname, 'client'),
-    devtool: 'inline-source-map',
-    devServer: {
-        hot: true,
-        contentBase: path.join(__dirname, 'dist'),
-        publicPath: '/',
-        historyApiFallback: {index: '/'}
-    },
-    module: {
-        rules: [{
-            test: /\.js$/,
-            use: [
-                'babel-loader',
-                'eslint-loader'
-            ],
-            exclude: /node_modules/
-        }, {
-            test: /\.scss$/,
-            use: [
-                'style-loader',
-                'css-loader',
-                'resolve-url-loader',
-                'sass-loader?sourceMap',
-                'postcss-loader'
-            ]
-        }, {
-            test: /(\.jpg$|\.png$)/,
-            use: 'file-loader'
-        }, {
-            test: /\.woff(2)?(\?v=[0-9]\.[0-9]\.[0-9])?$/,
-            use: 'url-loader?limit=10000&mimetype=application/font-woff'
-        }, {
-            test: /\.(ttf|eot|svg)(\?v=[0-9]\.[0-9]\.[0-9])?$/,
-            use: 'file-loader'
-        }]
-    },
-    plugins: [
-        new webpack.HotModuleReplacementPlugin(),
-        new webpack.NamedModulesPlugin()
-    ]
+module.exports = function (env) {
+    const config = {
+        output: {
+            filename: '[name]-[hash].js',
+            path: path.join(__dirname, 'dist'),
+            publicPath: '/'
+        },
+        context: path.join(__dirname, 'client'),
+        devtool: 'inline-source-map',
+        module: {
+            rules: [{
+                test: /\.js$/,
+                use: [
+                    'babel-loader',
+                    'eslint-loader'
+                ],
+                exclude: /node_modules/
+            }, {
+                test: /\.scss$/,
+                use: [
+                    'style-loader',
+                    'css-loader',
+                    'resolve-url-loader',
+                    'sass-loader?sourceMap',
+                    'postcss-loader'
+                ]
+            }, {
+                test: /(\.jpg$|\.png$)/,
+                use: 'file-loader'
+            }, {
+                test: /\.woff(2)?(\?v=[0-9]\.[0-9]\.[0-9])?$/,
+                use: 'url-loader?limit=10000&mimetype=application/font-woff'
+            }, {
+                test: /\.(ttf|eot|svg)(\?v=[0-9]\.[0-9]\.[0-9])?$/,
+                use: 'file-loader'
+            }]
+        },
+        plugins: [
+            new webpack.NamedModulesPlugin(),
+            new HtmlWebpackPlugin({template: 'index.html'})
+        ]
+    };
+
+    if (env !== 'production') {
+        config.entry = [
+            'react-hot-loader/patch',
+            'webpack-dev-server/client?http://localhost:8080',
+            'webpack/hot/only-dev-server',
+            './index.js'
+        ];
+        config.devServer = {
+            hot: true,
+            contentBase: path.join(__dirname, 'dist'),
+            publicPath: '/',
+            historyApiFallback: {index: '/'}
+        };
+        config.plugins.push(new webpack.HotModuleReplacementPlugin());
+    } else {
+        config.entry = [
+            './index.js'
+        ];
+    }
+
+    return config;
 };
