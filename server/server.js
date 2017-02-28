ee435263601d4db9a818e189b39c5250495a88c7
Oleksii <oleksii.kukuruza@gmail.com>
2017-02-28 15:28:26 +0200

    Configured heroku deployment (#9)
    * Configured heroku deployment
    
    * Removed index from dist
    
    * Added new line

--- server/server.js
+++ server/server.js
@@ -1,11 +1,17 @@
+import path from 'path';
 import Express from 'express';
+import fallback from 'express-history-api-fallback';
 
 const app = new Express();
 const port = 3000;
+const root = path.resolve(__dirname, '..', 'dist');
 
-app.get('/', (req, res) => {
+app.get('/api/', (req, res) => {
     res.send('Hello World!')
 });
+
+app.use(Express.static(root, {maxAge: '2d'}));
+app.use(fallback('index.html', {root}));
 
 app.listen(port, (error) => {
     if (error) {
