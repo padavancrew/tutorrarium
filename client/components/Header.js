e90ad87390db5bce464ca070bdb2b020719d8046
Oleksandr <san9.m92@gmail.com>
2017-03-04 17:00:25 +0200

    test

--- client/components/Header.js
+++ client/components/Header.js
@@ -1,10 +1,20 @@
-import React, { Component } from 'react';
+import React, {Component} from 'react';
+import './Header.scss';
 
 export class Header extends Component {
     render() {
         return (
-            <div>
-                <h1>1_HEADER_PLACEHOLDER</h1>
+            <div className="header">
+                <img className="header-logo" src={require('../img/logo.png')}/>
+                <span className="header-title">TUTORRARIUM</span>
+
+                <div className="header-navBlock">
+                <div className="header-navButton">Home </div>
+                <div className="header-navButton"> Contact</div>
+                <div className="header-navButton"> About</div>
+                <div className="header-navButton"> Links</div>
+                <div className="header-navButton"> Services </div>
+            </div>
             </div>
         );
     }
