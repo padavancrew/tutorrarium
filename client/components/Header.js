72fb1850879336f437dc98b1f010c16413fce9e0
Oleksandr <san9.m92@gmail.com>
2017-03-05 20:30:20 +0200

    test

--- client/components/Header.js
+++ client/components/Header.js
@@ -7,14 +7,12 @@ export class Header extends Component {
             <div className="header">
                 <img className="header-logo" src={require('../img/logo.png')}/>
                 <span className="header-title">TUTORRARIUM</span>
-
-                <div className="header-navBlock">
-                <div className="header-navButton">Home </div>
-                <div className="header-navButton"> Contact</div>
-                <div className="header-navButton"> About</div>
-                <div className="header-navButton"> Links</div>
-                <div className="header-navButton"> Services </div>
-            </div>
+                <ul className="navigation">
+                    <li><a href="">Home</a></li>
+                    <li><a href="">About</a></li>
+                    <li><a href="">Products</a></li>
+                    <li><a href="">Contact</a></li>
+                </ul>
             </div>
         );
     }
