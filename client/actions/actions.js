49980f8adea75886369e0cc391fa40a9ed37328e
Edgar000 <edgar026026@gmail.com>
2017-03-30 17:11:03 +0300

    cleaning, new feature init

--- client/actions/actions.js
+++ client/actions/actions.js
@@ -1,9 +1,8 @@
 import * as types from '../constants/ActionTypes';
 
-export function addProblem(subject, description) {
+export function addProblem(problem) {
     return {
         type: types.ADD_PROBLEM,
-        subject,
-        description
+        problem
     }
 }
